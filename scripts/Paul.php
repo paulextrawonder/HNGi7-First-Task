49f3acc42b40cdee01aa4093fa57bdc7f37df513
PaulHack <54580197+paulextrawonder@users.noreply.github.com>
2020-06-05 09:45:17 +0100

    Added email address and corrected text casing

--- scripts/Paul.php
+++ scripts/Paul.php
@@ -4,5 +4,5 @@ $hng_id = 'HNG-05972';
 $language = 'PHP';
 $email = 'paulextrawonder@gmail.com';
 
-echo 'Hello World, this is ' .$full_name . ' with HNGi7 ID ' .$hng_id . ' using ' .$language .' for stage 2 task and this is my email address ' .$email ;
+echo 'Hello World, this is ' .$full_name . ' with HNGi7 ID ' .$hng_id . ' using ' .$language .' for stage 2 and ' .$email ;
 ?>
